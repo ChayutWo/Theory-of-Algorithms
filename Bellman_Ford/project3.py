05f8bb459a40d87299df09095c0ee34983e3d1a3
Chayut Wongkamthong <cw403@duke.edu>
2019-11-06 22:38:14 -0500

    update comments

--- Bellman_Ford/project3.py
+++ Bellman_Ford/project3.py
@@ -19,8 +19,8 @@ detectArbitrage
 Objective: Perform Bellman-Ford Algorithm to detect Arbitrage
 Input: currencies - the Currencies object for the exchange rate
        tol - update tolerance
-Expected output: A list containing a rank of vertices that result in Arbitrage
-                 Note that if Arbitrage doesn't exist, the list will be empty
+Expected output: A list containing ranks of vertices that result in Arbitrage
+                 If Arbitrage doesn't exist, return empty list
 """
 def detectArbitrage(currencies, tol=1e-15):
     ##### Your implementation goes here. #####
@@ -32,42 +32,52 @@ def detectArbitrage(currencies, tol=1e-15):
     # Assume that we start at the first vertex
     currencies.adjList[0].dist = 0
 
-    # Iterate |V| times through the vertex
-    for i in range(len(currencies.adjList)):
-        # Initialize change and changeVertex to keep track of the change in dist
-        # This will be used to determine arbitrage in the last loop
-        change = False
-        changeVertex = None
-
+    # Part I: Perform Bellman-Ford |V|-1 iterations.
+    for i in range(len(currencies.adjList)-1):
         # for each vertex in the currencies graph, make offers to neighbors
         for vertex in currencies.adjList:
             for neighbor in vertex.neigh:
-                # If the current offer is better than the previous prediction,
-                # update dist, and prev of that neighbor
-                # update change and changeVertex
+                # If the offer is better than the prediction, update parameters.
                 distance = currencies.adjMat[vertex.rank][neighbor.rank]
                 if neighbor.dist > vertex.dist + distance + tol:
                     neighbor.dist = vertex.dist + distance
                     neighbor.prev = vertex
-                    change = True
-                    changeVertex = neighbor
+
+    # Part II: Perform Bellman-Ford 1 iteration to find changes.
+    # Initialize change and changeVertex to keep track of the change in dist
+    change = False
+    changeVertex = None
+    # for each vertex in the currencies graph, make offers to neighbors
+    for vertex in currencies.adjList:
+        for neighbor in vertex.neigh:
+            # If the offer is better than the prediction, update parameters.
+            distance = currencies.adjMat[vertex.rank][neighbor.rank]
+            if neighbor.dist > vertex.dist + distance + tol:
+                neighbor.dist = vertex.dist + distance
+                neighbor.prev = vertex
+                # Record change and the vertex that is updated
+                change = True
+                changeVertex = neighbor
 
     cycle = [] # Initialize the cycle list for output
     # If there are changes in the last loop, there is arbitrage
-    # Backtrack using prev value until we found a loop
     if change:
+        # Part III: Backtrack the path of the changed vertex until find cycle
         while changeVertex.rank not in cycle:
             cycle = [changeVertex.rank] + cycle
             changeVertex = changeVertex.prev
 
-        # Extract only part of the list that is cycle
+        # Part IV: Extract only part of the list that is cycle
         # Find position where rank is the same as the start of the loop
         foundLoopEnd = False
         index = 0
         while not foundLoopEnd:
+            # If, at that index, the vertex is the end of loop, stop.
+            # Else, continue searching.
             if cycle[index]== changeVertex.rank:
                 foundLoopEnd = True
-            index += 1
+            else:
+                index += 1
         cycle = [changeVertex.rank] + cycle[:index+1]
     return cycle
     ##### Your implementation goes here. #####
@@ -82,7 +92,6 @@ Expected Output: an adjacency matrix of exchange rates
 """
 def rates2mat(rates):
     ##### Your implementation goes here. #####
-    # Currently this only returns a copy of the rates matrix.
     # For each rate in rates, store -log(rate) in the adjacency matrix
     return [[-math.log(R) for R in row] for row in rates]
     ##### Your implementation goes here. #####
